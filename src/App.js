92b7ce649abb01ce841ef4cd729ced4ef72d31d0
Retno Ayu Adiati <retno_ayu@hotmail.com>
2020-10-26 23:23:32 +0100

    Add Footer, add style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
